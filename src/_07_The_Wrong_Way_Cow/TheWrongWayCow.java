36baab964c0b3100336bbf5825776a01d15ece11
AVillarreal2 <30839429+AVillarreal2@users.noreply.github.com>
2023-05-20 11:29:46 -0700

    uhhh why is the code not working
    bruh, figure it out and finish the thingymajig

--- src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
+++ src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
@@ -58,21 +58,33 @@ public class TheWrongWayCow {
 
         //how to find the wrong way cow:
         //1. go through all of the characters, check if it is "c"
-        //2. if it is, look at all of the different sides of where the "o" is (N,E,S,W,SW,NW,SE,NE)
+        //2. if it is, look at all of the different sides of where the "o" is
         //3. add it to a list of which if the first two are the same keep looking until there is a different one,
         // and if there is already a different one just give the location of that one.
+
+        // NW N NE     [i-1][j-1]  [i-1][j]  [i-1][j+1]
+        // W  c  E  =  [i][j-1]     [i][j]     [i][j+1]
+        // SW S SE     [i+1][j-1]  [i+1][j]  [i+1][j+1]
 
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
                 if (Objects.equals(field[i][j], "c")) {
-                    if(Objects.equals(field[i][j], "o")) {
-                    }else if(Objects.equals(field[i][j], "o")) {
-                    }else if(Objects.equals(field[i][j], "o")) {
-                    }else if(Objects.equals(field[i][j], "o")) {
-                    }else if(Objects.equals(field[i][j], "o")) {
-                    }else if(Objects.equals(field[i][j], "o")) {
-                    }else if(Objects.equals(field[i][j], "o")) {
-                    }else if(Objects.equals(field[i][j], "o")) {
+                    if(Objects.equals(field[i-1][j-1], "o")) {
+                        System.out.println("o = NW");
+                    }else if(Objects.equals(field[i-1][j], "o")) {
+                        System.out.println("o = N");
+                    }else if(Objects.equals(field[i-1][j+1], "o")) {
+                        System.out.println("o = NE");
+                    }else if(Objects.equals(field[i][j-1], "o")) {
+                        System.out.println("o = W");
+                    }else if(Objects.equals(field[i][j+1], "o")) {
+                        System.out.println("o = E");
+                    }else if(Objects.equals(field[i+1][j-1], "o")) {
+                        System.out.println("o = SW");
+                    }else if(Objects.equals(field[i+1][j], "o")) {
+                        System.out.println("o = S");
+                    }else if(Objects.equals(field[i+1][j+1], "o")) {
+                        System.out.println("o = SE");
                     }
                 }
             }
