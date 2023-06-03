8ca21cd54de83fbc83dcbb59849f8d2cda926b92
AVillarreal2 <30839429+AVillarreal2@users.noreply.github.com>
2023-06-03 11:27:22 -0700

    finish the logic
    contemplating whether i did it right

--- src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
+++ src/_07_The_Wrong_Way_Cow/TheWrongWayCow.java
@@ -66,29 +66,57 @@ public class TheWrongWayCow {
         // W  c  E  =  [i][j-1]     [i][j]     [i][j+1]
         // SW S SE     [i+1][j-1]  [i+1][j]  [i+1][j+1]
 
+        int cx1 = 0;
+        int cy1 = 0;
+        int ox1 = 0;
+        int oy1 = 0;
+
+        int cx2 = 0;
+        int cy2 = 0;
+        int ox2 = 0;
+        int oy2 = 0;
+
+        //
+
         for (int i = 0; i < field.length; i++) {
             for (int j = 0; j < field[i].length; j++) {
-                if (Objects.equals(field[i][j], "c")) {
-                    if(Objects.equals(field[i-1][j-1], "o")) {
-                        System.out.println("o = NW");
-                    }else if(Objects.equals(field[i-1][j], "o")) {
-                        System.out.println("o = N");
-                    }else if(Objects.equals(field[i-1][j+1], "o")) {
-                        System.out.println("o = NE");
-                    }else if(Objects.equals(field[i][j-1], "o")) {
-                        System.out.println("o = W");
-                    }else if(Objects.equals(field[i][j+1], "o")) {
-                        System.out.println("o = E");
-                    }else if(Objects.equals(field[i+1][j-1], "o")) {
-                        System.out.println("o = SW");
-                    }else if(Objects.equals(field[i+1][j], "o")) {
-                        System.out.println("o = S");
-                    }else if(Objects.equals(field[i+1][j+1], "o")) {
-                        System.out.println("o = SE");
+                if (Objects.equals(field[i][j], "c")) { //checks if [i][j] is c
+                    for (int k = -1; k < 1; k++) {
+                        for (int l = -1; l < 1; l++) {
+                            if (Objects.equals(field[i + k][j + l], "o")) { //runs through a 3x3 around the c
+                                ox1 = cx1 + k;
+                                oy1 = cy1 + l;
+                            }
+                        }
+//                    if(Objects.equals(field[i-1][j], "o")) { //if it is, checks if o is N
+//                        ox1 = cx1-1;
+//                        oy1 = cy1;
+//                    }else if(Objects.equals(field[i-1][j-1], "o")) { //if it is, checks if o is NW
+//                        ox1 = cx1-1;
+//                        oy1 = cy1-1;
+//                    }else if(Objects.equals(field[i-1][j+1], "o")) { //if it is, checks if o is NE
+//                        ox1 = cx1-1;
+//                        oy1 = cy1+1;
+//                    }else if(Objects.equals(field[i][j-1], "o")) { //if it is, checks if o is W
+//                        ox1 = cx1;
+//                        oy1 = cy1-1;
+//                    }else if(Objects.equals(field[i][j+1], "o")) { //if it is, checks if o is E
+//                        ox1 = cx1;
+//                        oy1 = cy1+1;
+//                    }else if(Objects.equals(field[i+1][j-1], "o")) { //if it is, checks if o is SW
+//                        ox1 = cx1+1;
+//                        oy1 = cy1-1;
+//                    }else if(Objects.equals(field[i+1][j], "o")) { //if it is, checks if o is S
+//                        ox1 = cx1+1;
+//                        oy1 = cy1;
+//                    }else if(Objects.equals(field[i+1][j+1], "o")) { //if it is, checks if o is SE
+//                        ox1 = cx1+1;
+//                        oy1 = cy1+1;
+//                    }
                     }
                 }
             }
         }
-        return null;
+        return new int[]{cx2, cy2}; //actually return something
     }
 }
